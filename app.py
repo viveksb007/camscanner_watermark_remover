9a338444421e5a839f462f9a26417304de782a11
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2018-03-21 22:44:21 +0530

    Flask app completed

--- app.py
+++ app.py
@@ -1,11 +1,64 @@
 import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
-UPLOAD_FOLDER = ''
-ALLOWED_EXTENSIONS = {'pdf'}
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
+DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads/'
+ALLOWED_EXTENSIONS = {'pdf', 'txt'}
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+# limit upload size upto 8mb
+app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
 
-# http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print('No file attached in request')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            print('No file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), filename)
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('index.html')
+
+
+def process_file(path, filename):
+    remove_watermark(path, filename)
+    # with open(path, 'a') as f:
+    #    f.write("\nAdded processed content")
+
+
+def remove_watermark(path, filename):
+    input_file = PdfFileReader(open(path, 'rb'))
+    output = PdfFileWriter()
+    for page_number in range(input_file.getNumPages()):
+        page = input_file.getPage(page_number)
+        page.mediaBox.lowerLeft = (page.mediaBox.getLowerLeft_x(), 20)
+        output.addPage(page)
+    output_stream = open(app.config['DOWNLOAD_FOLDER'] + filename, 'wb')
+    output.write(output_stream)
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
